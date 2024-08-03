033c8078e9d16e4ec068d5ac074bbddb3af65cba
Faheem Ashkar M R <faheem.ashkar@gmail.com>
2024-08-03 11:58:43 +0530

    Adaptations to etched board

--- button_lora_rx.ino
+++ button_lora_rx.ino
@@ -1,81 +1,79 @@
-#include "Arduino.h"
 #include "LoRaWan_APP.h"
+
+// LoRa parameters
+#define RF_FREQUENCY  915000000  // Hz
+#define RX_TIMEOUT_VALUE  1000
+#define BUFFER_SIZE  30  // Define the payload size here
 #include <Wire.h>
 #include "HT_SSD1306Wire.h"
 
 // OLED f_display setup
 SSD1306Wire f_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED);
 
-// LoRa parameters
-#define RF_FREQUENCY  915000000  // Hz
-#define RX_TIMEOUT_VALUE  1000
-#define BUFFER_SIZE  30  // Define the payload size here
-
 char rxpacket[BUFFER_SIZE];  // Buffer for the received packet
 bool lora_idle = true;
 static RadioEvents_t RadioEvents;
+
 void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr);
 void OnRxError(void);
 
 void setup() {
-    Serial.begin(115200);
-    Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
-    pinMode(46, OUTPUT);  // Setup for a relay
-
-    f_display.init();
-    f_display.flipScreenVertically();
-    f_display.setFont(ArialMT_Plain_16);
-    f_display.drawString(0, 0, "Waiting...");
-    f_display.display();
-    digitalWrite(46, HIGH);  // Turn off the relay
-     
-    // Initialize Radio events
-    RadioEvents.RxDone = OnRxDone;
-    RadioEvents.RxError = OnRxError;
-    Radio.Init(&RadioEvents);
-    Radio.SetChannel(RF_FREQUENCY);
-    Radio.SetRxConfig(MODEM_LORA, 0, 0x0c,4 , 0, 8, 0, false, 0, true, 0, 0, false, true);
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
+  // Initialize Radio events
+  f_display.init();
+  f_display.flipScreenVertically();
+  f_display.setFont(ArialMT_Plain_16);
+  f_display.drawString(0, 0, "Waiting...");
+  f_display.display();
+  pinMode(45, OUTPUT);  // Set the button pin as input
+  digitalWrite(45, LOW);
+  RadioEvents.RxDone = OnRxDone;
+  RadioEvents.RxError = OnRxError;
+  Radio.Init(&RadioEvents);
+  Radio.SetChannel(RF_FREQUENCY);
+  Radio.SetRxConfig(MODEM_LORA, 0, 0x0c,4 , 0, 8, 0, false, 0, true, 0, 0, false, true);
 }
 
 void loop() {
-   if(lora_idle)
-  {
-    lora_idle = false;
-    Serial.println("into RX mode");
-    Radio.Rx(0);
-  }
+  // put your main code here, to run repeatedly:
+  if(lora_idle)
+    {
+      lora_idle = false;
+      Serial.println("into RX mode");
+      Radio.Rx(0);
+    }
   Radio.IrqProcess( );
 }
 
 void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr) {
     memcpy(rxpacket, payload, size);
     rxpacket[size] = '\0';  // Null-terminate the string
-
+    Serial.printf("Received '%s' with RSSI %d\n", rxpacket, rssi);
     f_display.clear();
     //f_display.drawString(0, 0, "Received:");
     f_display.drawString(0, 16, String(rssi));
     f_display.display();
 
     if (strcmp(rxpacket, "ON") == 0) {
-        digitalWrite(46, LOW);  // Turn on the relay
+        
         f_display.drawString(0, 32, "Relay ON!");
-        //delay(1000);
+        digitalWrite(45, HIGH);
     } else {
-        digitalWrite(46, HIGH);  // Turn off the relay
+        digitalWrite(45, LOW);
         f_display.drawString(0, 32, "Relay OFF!");
     }
     f_display.display();
-
-    Serial.printf("Received '%s' with RSSI %d\n", rxpacket, rssi);
     Radio.Sleep();
-     lora_idle = true;
+    lora_idle = true;
 }
 
 void OnRxError(void) {
-    f_display.clear();
-    f_display.drawString(0, 0, "RX Error!");
-    f_display.display();
-
-    Serial.println("RX Error...");
-    Radio.Sleep();
+  Serial.println("RX Error...");
+  f_display.clear();
+  f_display.drawString(0, 0, "RX Error!");
+  f_display.display();
+  Radio.Sleep();
+  lora_idle = true;
 }
--- button_lora_tx.ino
+++ button_lora_tx.ino
@@ -1,11 +1,12 @@
-#include "Arduino.h"
 #include "LoRaWan_APP.h"
-#include <Wire.h>
-#include "HT_SSD1306Wire.h"
 
-// OLED display setup
-SSD1306Wire f_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED);
-
+#define LED_R 46
+#define LED_G 48
+#define LED_B 47
+#define ADC_CTRL 37
+#define BATT_ADC_IN 1
+#define BLUE_LED_TIME 500
+#define RED_LED_TIME 100
 // LoRa parameters
 #define RF_FREQUENCY  915000000  // Hz
 #define TX_OUTPUT_POWER  22  // dBm
@@ -17,29 +18,37 @@ SSD1306Wire f_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLE
 #define LORA_SYMBOL_TIMEOUT  0
 #define LORA_FIX_LENGTH_PAYLOAD_ON  false
 #define LORA_IQ_INVERSION_ON  false
+#define LORA_SEND_INTVL 1500
+#define LORA_RLY_INTVL 500
 
+int blue_led_st = 255;
+int red_led_st = 255;
+bool charging_st;
+double charging_led_int = 0;
+double low_batt_led_int = 0;
+double lora_send_intvl = 0;
+double lora_rly_intvl = 0;
+int red_val = 0;
+int green_val = 0;
 char txpacket[30]; // Define the payload size here
-double txNumber;
+int txNumber;
 static RadioEvents_t RadioEvents;
+int button_state_prev = LOW;
+
 void OnTxDone(void);
 void OnTxTimeout(void);
 
-bool lora_idle=true;
 void setup() {
-    Serial.begin(115200);
-    Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
-    pinMode(45, INPUT);  // Set the button pin as input
-   // lora_idle == true
-    f_display.init();
-    f_display.flipScreenVertically();
-    f_display.setFont(ArialMT_Plain_16);
-    f_display.drawString(0, 0, "Connecting...");
-    f_display.display();
-
-    // Initialize Radio events
-    RadioEvents.TxDone = OnTxDone;
-    RadioEvents.TxTimeout = OnTxTimeout;
-
+  // put your setup code here, to run once:
+  Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
+  pinMode(ADC_CTRL, OUTPUT);
+  digitalWrite(ADC_CTRL, LOW);
+  pinMode(BATT_ADC_IN, INPUT);
+  // Initialize Radio events
+  pinMode(45, INPUT);  // Set the button pin as input
     Radio.Init(&RadioEvents);
     Radio.SetChannel(RF_FREQUENCY);
     Radio.SetTxConfig(MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
@@ -49,52 +58,73 @@ void setup() {
 }
 
 void loop() {
-    int buttonState = digitalRead(45);  // Read the state of the button
-
-
-     if (buttonState == HIGH) {  // If the button is pressed
-      Serial.printf("button high");
-        sprintf(txpacket, "ON");  // Prepare the ON packet
-        f_display.clear();
-        f_display.drawString(0, 0, "Sending ON...");
-        f_display.display();
-        Serial.printf("\r\nsending packet \"%s\" , length %d\r\n",txpacket, strlen(txpacket));
-
-        Radio.Send((uint8_t *)txpacket, strlen(txpacket));  // Send the ON packet
-
-        //delay(2000);  // Debouncing delay
-    } else{
-       f_display.clear();
-       f_display.drawString(0, 0, "Relay off");
-       f_display.display();
-    delay(1000);
-    txNumber += 0.01;
-    sprintf(txpacket,"Hello world number %0.2f",txNumber);  //start a package
-     
-    Serial.printf("\r\nsending packet \"%s\" , length %d\r\n",txpacket, strlen(txpacket));
-
-    Radio.Send( (uint8_t *)txpacket, strlen(txpacket) ); //send the package out 
+  int buttonState = digitalRead(45);  // Read the state of the button
+  // put your main code here, to run repeatedly:
+  int analogValue = analogRead(BATT_ADC_IN);
+  float map_val = (analogValue * 4.12) / 980;
+  if (map_val >= 3.9) {
+    charging_st = true;
+  }
+  else {
+    charging_st = false;
+  }
+  
+  if (charging_st == true) {  //fn to handle LED during charging
+    analogWrite(LED_R, 0);
+    analogWrite(LED_G, 0);
+    if (millis() - charging_led_int > BLUE_LED_TIME) {
+      analogWrite(LED_B, blue_led_st);
+      charging_led_int = millis();
+      blue_led_st = 255 - blue_led_st;
     }
-}
-
-void OnTxDone(void) {
-
-
-    Serial.println("TX done......");
-    //delay(2000); // Delay to view message
-    lora_idle = true;
-
-}
-
-void OnTxTimeout(void) {
-    f_display.clear();
-    f_display.drawString(0, 0, "TX Timeout...");
-    f_display.display();
-
-    Serial.println("TX Timeout......");
-    //delay(2000); // Delay to view message
-
-    f_display.clear();
-    f_display.drawString(0, 0, "Ready");
-    f_display.display();
+  }
+  else {  //fn to handle LED during discharging
+    if (map_val > 3.75) { //full charge
+        analogWrite(LED_B, 0);
+        analogWrite(LED_R, 0);
+        analogWrite(LED_G, 255);
+    }
+    else if (map_val < 3.31)  //critical battery level
+    {
+        analogWrite(LED_B, 0);
+        analogWrite(LED_G, 0);
+      if (millis() - low_batt_led_int > RED_LED_TIME) {
+        analogWrite(LED_R, red_led_st);
+        low_batt_led_int = millis();
+        red_led_st = 255 - red_led_st;
+    }
+    }
+    else {
+      if (map_val >= 3.53) {  //above half charge
+        red_val = 255 - (((map_val - 3.53) * 255) / 0.22);
+        green_val = 255;
+      }
+      else {  //below half charge
+        green_val = ((map_val - 3.31) * 255) / 0.21;
+        red_val = 255;
+      }
+      analogWrite(LED_B, 0);
+      analogWrite(LED_G, green_val);
+      analogWrite(LED_R, red_val);
+    }
+  }
+  if (buttonState == HIGH) {  // If the button is pressed
+    if ((millis() - lora_rly_intvl > LORA_RLY_INTVL)) {
+      lora_rly_intvl = millis();
+      sprintf(txpacket, "ON");  // Prepare the ON packet
+      Radio.Send((uint8_t *)txpacket, strlen(txpacket));  // Send the ON packet
+      button_state_prev = buttonState;
+    }
+  }
+  else if ((millis() - lora_send_intvl > LORA_SEND_INTVL) or (buttonState == LOW && button_state_prev == HIGH)) {
+    lora_send_intvl = millis();
+    txNumber += 1;
+    sprintf(txpacket,"%0.2f", txNumber);  //start a package
+    //sprintf(txpacket, "Battery level: %0.2f", map_val);  // Prepare the send packet
+    Radio.Send((uint8_t *)txpacket, strlen(txpacket));  // Send the batt voltage packet
+    button_state_prev = buttonState;
+  }
+  else {
+    button_state_prev = buttonState;
+  }
 }
